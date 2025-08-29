8df7a724f0b794a0421b675294deefa1b311e38e
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2025-08-29 12:19:06 +0330

    add/deploy-setting

--- src/services/cryptoServices.ts
+++ src/services/cryptoServices.ts
@@ -1,7 +1,26 @@
-import axios from "axios";
-import { Crypto } from "@/lib/types";
+// import axios from "axios";
+// import { Crypto } from "@/lib/types";
 
-export const fetchCryptos = async (start = 1, limit = 50): Promise<Crypto[]> => {
-  const res = await axios.get(`/api/cryptos`, { params: { start, limit } });
-  return res.data as Crypto[];
-};
+// export const fetchCryptos = async (start = 1, limit = 50): Promise<Crypto[]> => {
+//   const res = await axios.get(`/api/cryptos`, { params: { start, limit } });
+//   return res.data as Crypto[];
+// };
+
+export async function fetchCryptos(start: number, limit: number) {
+  const apiKey = process.env.COINMARKETCAP_API_KEY; // امن و فقط سمت سرور
+
+  const res = await fetch(
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=${start}&limit=${limit}`,
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": apiKey || "",
+      },
+      next: { revalidate: 60 }, // ISR برای کش
+    }
+  );
+
+  if (!res.ok) throw new Error("Failed to fetch cryptos");
+  const data = await res.json();
+  return data.data; // فقط آرایه کریپتوها
+}
+
